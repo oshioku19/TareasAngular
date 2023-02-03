559b49091e6ccf5f8ff2fcb1d81eebb7e755e6e6
oshioku19 <oshioku@outlook.com.pe>
2023-02-03 02:45:31 -0500

    v2f

--- Sesión14/TareaAsincrona/karma.conf.js
+++ Sesión14/TareaAsincrona/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/actividad-14'),
+      dir: require('path').join(__dirname, './coverage/Tarea-Asincrona'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- Sesión14/TareaAsincrona/src/app/app.component.spec.ts
+++ Sesión14/TareaAsincrona/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Actividad-14'`, () => {
+  it(`should have as title 'Tarea-Asincrona'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Actividad-14');
+    expect(app.title).toEqual('Tarea-Asincrona');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Actividad-14 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Tarea-Asincrona app is running!');
   });
 });
